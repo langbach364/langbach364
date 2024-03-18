f5d21b59bf04fe26dfa6e7e2a3690a1bb469aba0
langbach364 <96160493+langbach364@users.noreply.github.com>
2024-03-18 14:52:25 +0700

    delete code

--- Server/Create-server.go
+++ Server/Create-server.go
@@ -25,80 +25,8 @@ func enable_middleware_cors(next http.Handler) http.Handler {
 	})
 }
 
-func Router_sign_up(router *http.ServeMux) {
-	router.HandleFunc("/SignUp", func(w http.ResponseWriter, r *http.Request) {
-		w.Header().Set("Content-Type", "application/json")
-		switch r.Method {
-		case "POST":
-			data, err := io.ReadAll(r.Body)
-			check_err(err)
-			check := sign_Up(data)
-			fmt.Fprintln(w, check)
-		case "GET":
-			fmt.Println("Get method is not used")
-		}
-	})
-}
-
-func Router_sign_in(router *http.ServeMux) {
-	router.HandleFunc("/SignIn", func(w http.ResponseWriter, r *http.Request) {
-		w.Header().Set("Content-Type", "application/json")
-		switch r.Method {
-		case "POST":
-			data, err := io.ReadAll(r.Body)
-			check_err(err)
-
-			check := sign_In(data)
-			fmt.Fprintln(w, check)
-		case "GET":
-			fmt.Println("Get method is not used")
-		}
-	})
-}
-
-func Router_handle_search(router *http.ServeMux) {
-	router.HandleFunc("/HandleSearch", func(w http.ResponseWriter, r *http.Request) {
-		w.Header().Set("Content-Type", "application/json")
-		switch r.Method {
-		case "POST":
-			data, err := io.ReadAll(r.Body)
-			check_err(err)
-
-			result := handle_search(data)
-			fmt.Fprintln(w, result)
-		case "GET":
-			fmt.Println("Get method is not used")
-		}
-	})
-}
-
-func Router_view(router *http.ServeMux) {
-	router.HandleFunc("/View", func(w http.ResponseWriter, r *http.Request) {
-		w.Header().Set("Content-Type", "application/json")
-		switch r.Method {
-		case "POST":
-			data, err := io.ReadAll(r.Body)
-			check_err(err)
-
-			result := view_more(data)
-			fmt.Fprintln(w, result)
-		case "GET":
-			fmt.Println("Get method is not used")
-		}
-	})
-}
-
-
-func muxtiplexer_router(router *http.ServeMux) {
-	Router_sign_up(router)
-	Router_sign_in(router)
-	Router_handle_search(router)
-	Router_view(router)
-}
-
 func Create_server() {
 	router := http.NewServeMux()
-	muxtiplexer_router(router)
 
 	router.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
 		w.Header().Set("Content-Type", "application/json")
