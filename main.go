7be7f2f2545e6b14ee3e8b34c5d5ff486462d20c
langbach <bachlang364@gmail,com>
2024-03-26 16:37:02 +0700

    fix bug

---
+++ main.go
@@ -0,0 +1,7 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println("algorithm after security:", encode_data1("11293088888888888888890165964823732443241123", "412344444444443@@@@@@@@@@@@#4411&^*&^*(^*&)$^!@#*()4!@#$21341241234", 1))
+}
